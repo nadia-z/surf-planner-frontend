c7e2bc3442f8e2ae22ac20571f1d58e0499a8c2f
Erik <erik.hoelzel@posteo.de>
2025-06-02 11:34:31 +0100

    made it run localy and with docker

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
+import React, { useState } from "react";
 import SurfPlanView from "./pages/SurfPlanView";
-import { DndProvider } from 'react-dnd';
-import { HTML5Backend } from 'react-dnd-html5-backend';
+import GuestListView from "./pages/GuestListView";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 
 function App() {
-  return (
-    <DndProvider backend={HTML5Backend}>
-       <SurfPlanView />
-    </DndProvider>
-  );
+    const [view, setView] = useState("surf");
+
+    return (
+        <DndProvider backend={HTML5Backend}>
+            <div className="container mt-3">
+                <div className="btn-group mb-4">
+                    <button
+                        className={`btn btn-${view === "surf" ? "primary" : "outline-primary"}`}
+                        onClick={() => setView("surf")}
+                    >
+                        Surf Plan
+                    </button>
+                    <button
+                        className={`btn btn-${view === "guest" ? "primary" : "outline-primary"}`}
+                        onClick={() => setView("guest")}
+                    >
+                        Guest Diet List
+                    </button>
+                </div>
+
+                {view === "surf" && <SurfPlanView />}
+                {view === "guest" && <GuestListView />}
+            </div>
+        </DndProvider>
+    );
 }
 
 export default App;
--- src/pages/GuestListView.jsx
+++ src/pages/GuestListView.jsx
@@ -4,7 +4,6 @@ import axios from "axios";
 import * as XLSX from "xlsx";
 import "../components/guestList/guestList.css";
 
-//const API_BASE_URL = "http://localhost:8000";
 const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
 
